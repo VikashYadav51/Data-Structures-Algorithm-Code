811dbc00118178bbf0a1b00c056aedf238ac0e78
Vikash Yadav <vikashyadavg012@gmail.com>
2025-09-09 09:13:53 +0530

    Add files via upload

---
+++ L6_Sorting_problem.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+void selection_sorting(int arr[], int size){
+    if(size == 0 || size ==1){
+        return ;
+    }
+
+    for(int i = 0; i < size; i++){
+        int index = i;
+        for(int j = i+1; j < size; j++){
+            if(arr[index] > arr[j]){
+                index = j;
+            }
+        }
+        swap(arr[index], arr[i]);
+    }
+}
+
+void bubble_sorting(int arr[], int size){
+    if(size == 0 || size == 1){
+        return ;
+    }
+
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size - 1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+            }
+        }
+    }
+}
+
+void insertion_sorting(int arr[], int size){
+    if(size==0 || size==1){
+        return ;
+    }
+
+    for(int i = 0; i < size; i++){
+        int key = arr[i];
+        int j =i - 1;
+
+        while( j >= 0){
+            if(arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            else{
+                break;
+            }
+         
+        }
+        arr[j+1] = key;
+    }
+
+}
+
+int main(){
+    int arr[6] = {11,3,6,2,5,1};
+    int size = 6;
+    // selection_sorting(arr, size);
+    // cout<<"After Selection sorting the element -> "<< endl;
+
+    // for(int i = 0; i < size; i++){
+    //     cout<< arr[i]<<"  ";
+
+    // } 
+
+    // bubble_sorting(arr, size);
+    // cout<<"After bubble sorting the element -> "<< endl;
+    // for(int i = 0; i < size; i++){
+    //     cout<< arr[i]<<"  ";
+
+    // }
+    
+
+
+    insertion_sorting(arr, size);
+    cout<<"After Insertion sorting the element -> "<< endl;
+    for(int i = 0; i < size; i++){
+        cout<< arr[i]<<"  ";
+
+    }
+}
